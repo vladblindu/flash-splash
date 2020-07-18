403e28a89662a0cb3e33fb0a01f461e1695ac29b
vlad <vlad.blindu@gmail.com>
2020-07-18 12:01:44 +0300

    fixed dist file

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
+import { terser } from "rollup-plugin-terser"
 
 export default {
     input: 'src/index.js',
     output: [
         {
-            file: 'build/bundle.js',
+            file: 'build/index.js',
             format: 'cjs'
         }
     ],
-    plugins: [nodeResolve({
-        jsnext: true
-    })]
+    plugins: [
+        nodeResolve({
+            jsnext: true
+        }),
+        terser()
+    ]
 }
